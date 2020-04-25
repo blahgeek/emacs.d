0c835b7811c28fb0fea67b2491bdeabcc95f68e3
BlahGeek <i@BlahGeek.com>
2020-04-25 20:01:46 +0800

    Enable save-place-mode

--- init.el
+++ init.el
@@ -466,6 +466,7 @@
  '(make-backup-files nil)
  '(package-selected-packages
    '(gn-mode dumb-jump fringe-scale protobuf-mode lsp-java git-gutter-fringe all-the-icons exec-path-from-shell fcitx vimrc-mode fish-mode vterm gcmh counsel-dash eyebrowse fzf ag hl-todo dtrt-indent flycheck mode-icons evil-magit magit evil-vimish-fold vimish-fold diminish cmake-mode ivy lsp-ui company-box solarized-theme company-lsp company company-mode which-key use-package projectile lsp-mode evil-visual-mark-mode evil-surround evil-commentary))
+ '(save-place-mode t)
  '(scroll-bar-mode nil)
  '(scroll-margin 2)
  '(scroll-step 1)
