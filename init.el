dbf8ca24343ddfe756382518793c7cc8d6bf50cc
BlahGeek <i@BlahGeek.com>
2020-05-18 23:38:29 +0800

    settings

--- init.el
+++ init.el
@@ -19,6 +19,8 @@
   (require 'use-package))
 (setq use-package-always-ensure t)
 
+(setq comp-deferred-compilation t)
+
 (use-package diminish)
 
 ;; Some helper function
@@ -33,9 +35,9 @@
 
 (use-package switch-buffer-functions)
 
-(use-package exec-path-from-shell
-  :config
-  (when (my/macos-p)
+(when (my/macos-p)
+  (use-package exec-path-from-shell
+    :config
     (exec-path-from-shell-initialize)))
 
 (use-package all-the-icons
@@ -497,6 +499,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(blink-cursor-mode nil)
  '(c-basic-offset 4)
  '(c-default-style
