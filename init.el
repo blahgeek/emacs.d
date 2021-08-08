d1790e6ead915d181234698d4b6980127a2d1a9a
BlahGeek <i@blahgeek.com>
2021-08-08 15:12:01 +0800

    Defer jit-lock, improve performance

--- init.el
+++ init.el
@@ -1115,6 +1115,7 @@
    '(hscroll-step 1)
    '(scroll-conservatively 101)
    '(indent-tabs-mode nil)
+   '(jit-lock-defer-time 0.25)  ;; defer jit-lock, improve performance
    '(line-number-mode nil)
    '(save-place-mode t)
    '(scroll-margin 2)
