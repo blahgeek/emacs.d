2674b1b0dce13d2ebd78104e63aebd1bccdffea5
Yikai Zhao <yikai@z1k.dev>
2021-10-07 22:16:21 +0800

    Remove jit-lock-defer-time

--- init.el
+++ init.el
@@ -1241,7 +1241,6 @@
    '(hscroll-step 1)
    '(scroll-conservatively 101)
    '(indent-tabs-mode nil)
-   '(jit-lock-defer-time 0.1)  ;; defer jit-lock, improve performance
    '(line-number-mode nil)
    '(save-place-mode t)
    '(scroll-margin 2)
