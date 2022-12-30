a54d249d140f863530cd25ce317d0027ff5fdc91
Yikai Zhao <yikai@z1k.dev>
2022-12-31 01:14:57 +0800

    remove archey from greetings

--- xonshconf/greeting.py
+++ xonshconf/greeting.py
@@ -6,12 +6,9 @@ import time
 import subprocess
 
 def greeting():
-    for cmd in ('/usr/bin/archey3', '/usr/bin/archey'):
-        if os.path.exists(cmd):
-            subprocess.run([cmd])
-            return
     uname = os.uname()
     print()
     print(f'Machine:  {uname.sysname} {uname.release} {uname.nodename}')
     print(f'Date:     {time.ctime()}')
-    print(f'Uptime:   ' + subprocess.check_output(['uptime'], universal_newlines=True))
+    print(f'Uptime:   ' + subprocess.check_output(['uptime'], universal_newlines=True).strip())
+    print()
