dc7361438e8d15ab891a9c0add354864bbc42134
Yikai Zhao <yikai@z1k.dev>
2026-04-02 01:01:06 +0800

    cleanup home.nix. single file

--- nix/home.nix
+++ nix/home.nix
@@ -1,7 +1,8 @@
 {...}:
 
 let
-  pkgs = import (fetchTarball {
+  pkgs = origPkgs // myPkgs;
+  origPkgs = import (fetchTarball {
     # 2026.3.22
     url = "https://github.com/NixOS/nixpkgs/archive/cf87881886182975bef15495007d6580c4aa6450.tar.gz";
     sha256 = "0mxr1b4g6wfbdsyz029ibc6pah43ci0cjl3k13y4i8k3z9sra9by";
@@ -16,22 +17,133 @@ let
       }))
     ];
   };
-  # flake-compat = import (fetchTarball {
-  #   # 2026.01.24
-  #   url = "https://github.com/edolstra/flake-compat/archive/5edf11c44bc78a0d334f6334cdaf7d60d732daab.tar.gz";
-  #   sha256 = "0yqfa6rx8md81bcn4szfp0hjq2f3h9i8zjzhqqyfqdkrj5559nmw";
-  # });
+
+  myPkgs = {
+
+    jujutsu = pkgs.rustPlatform.buildRustPackage rec {
+      pname = "jujutsu";
+      version = "0.39.0-master-lfs";
+      src = pkgs.fetchFromGitHub {
+        owner = "blahgeek";
+        repo = "jujutsu";
+        rev = "5e3a5acfe635c4ff6bbaba3fdb9d392ad586b5c9";  # 0.39.0-master-lfs
+        hash = "sha256-28+wlcA913fxJH3jtAXFh1oZm0uNTfJbbGPNSFrl9No=";
+      };
+      cargoHash = "sha256-R1ekt62wjM59qA2z22/2ljLmNAMHTzb1Ka1BG6ui3oc=";
+
+      doCheck = false;
+      cargoBuildFlags = [
+        # Don’t install the `gen-protos` build tool.
+        "--bin"
+        "jj"
+      ];
+    };
+
+    kimi-cli = (flake-compat {
+      src = fetchTarball {
+        url = "https://github.com/MoonshotAI/kimi-cli/archive/refs/tags/1.24.0.tar.gz";
+        sha256 = "1ds0w6m1nv2l84g0hd78ibgsy7sx0ys9qyrdcd1dgc3bzn6k5rm2";
+      };
+    }).defaultNix.packages.${pkgs.stdenv.hostPlatform.system}.default;
+
+    xonsh = (
+      let python312 = pkgs.python312.override {
+            packageOverrides = final: prev: rec {
+              xonsh = prev.xonsh.overridePythonAttrs (prev: rec {
+                # https://github.com/xonsh/xonsh/pull/6026
+                version = "0.22.0-fix-completer";
+                src = pkgs.fetchFromGitHub {
+                  owner = "blahgeek";
+                  repo = "xonsh";
+                  rev = "1e239faed7a16e1b098acb503f1d884e719e8607";
+                  hash = "sha256-EQiK1d60F/rHX1K+S6KqpzL82ssulECeasSiAkQ+Ah0=";
+                };
+                doCheck = false;
+              });
+            };
+          };
+      in
+        (origPkgs.xonsh.override {
+          python3 = python312;
+          extraPackages = ps: [
+            ps.xonsh.xontribs.xontrib-abbrevs
+            (with ps; buildPythonPackage {
+              pname = "xontrib-autojump";
+              version = "1.4";
+              src = pkgs.fetchFromGitHub {
+                owner = "wshanks";
+                repo = "xontrib-autojump";
+                tag = "v1.4";
+                hash = "sha256-IhF40olhMR5Ymu57kDu8jzD4QCjd6wMzHcsubNExpaA=";
+              };
+              pyproject = true;
+              build-system = [
+                setuptools
+              ];
+            })
+          ];
+        })
+    );
+
+    emacs-lsp-booster = (pkgs.rustPlatform.buildRustPackage rec {
+      pname = "emacs-lsp-booster";
+      version = "5f702a26";
+      src = pkgs.fetchFromGitHub {
+        owner = "blahgeek";
+        repo = "emacs-lsp-booster";
+        rev = "5f702a2699f306a3958ff1996a2b1a625f0cee0b";
+        hash = "sha256-R9v+hCma/FfYdR+fvZ0vmtVk4dm+bPBacwV1QCc6X+8=";
+      };
+      cargoHash = "sha256-qchwxW3KITQcv6EFzR2BSISWB2aTW9EdCN/bx5m0l48=";
+      doCheck = false;
+    });
+  };
+
+  flake-compat = import (fetchTarball {
+    # 2026.01.24
+    url = "https://github.com/edolstra/flake-compat/archive/5edf11c44bc78a0d334f6334cdaf7d60d732daab.tar.gz";
+    sha256 = "0yqfa6rx8md81bcn4szfp0hjq2f3h9i8zjzhqqyfqdkrj5559nmw";
+  });
+
+  mkWrapperWithEnv = name: pkg: envs : (pkgs.symlinkJoin {
+    name = "${name}-wrapped";
+    paths = [ pkg ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = "wrapProgram $out/bin/${name} " +
+                (pkgs.lib.concatStringsSep
+                  " "
+                  (pkgs.lib.mapAttrsToList (k: v: "--set ${k} ${v}") envs));
+  });
+  mkAgentTool = (name: pkg: pkgs.writeShellApplication {
+    name = name;
+    runtimeInputs = [ myScripts.emacs-get-gptel-api-key ];
+    text = ''
+      ${./etc/agent-tools}/sandbox-run ${./etc/agent-tools}/${name}.bash ${pkg}/bin/${name} "$@"
+    '';
+  });
+
+
+  gitconfig = pkgs.replaceVars ./etc/git/config {
+    gitignore = "${./etc/git/ignore}";
+  };
+  jjconfig = pkgs.replaceVars ./etc/jj/config.toml {
+    jjPushMr = "${./etc/jj/jj-push-mr}";
+    jjMagitDiffEditor = "${./etc/jj/jj-magit-diff-editor}";
+  };
+
+  myScripts = builtins.mapAttrs (name: _: pkgs.writeShellApplication {
+    name = name;
+    runtimeInputs = [];
+    text = builtins.readFile ./etc/my-scripts/${name};
+    checkPhase = "";
+  }) (pkgs.lib.filterAttrs (name: value: value == "regular") (builtins.readDir ./etc/my-scripts));
+
 in
+
 pkgs.buildEnv {
   name = "home";
   pathsToLink = ["/bin" "/etc" "/include" "/lib" "/libexec" "/sbin" "/share"];
   paths = [
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
 
     (pkgs.emacs-git-nox.override {
       withNativeCompilation = true;
@@ -40,7 +152,55 @@ pkgs.buildEnv {
       withCompressInstall = false;
     })
 
-    # pkgs.aider-chat
+    (mkAgentTool "claude" pkgs.claude-code)
+    (mkAgentTool "codex" pkgs.codex)
+    (mkAgentTool "gemini" pkgs.gemini-cli)
+    (mkAgentTool "kimi" pkgs.kimi-cli)
+    (
+      pkgs.writeShellApplication {
+        name = "aider";
+        runtimeInputs = [ myScripts.emacs-get-gptel-api-key pkgs.aider-chat ];
+        text = ''
+        export GIT_CONFIG_GLOBAL=${gitconfig}
+        # shellcheck source=/dev/null
+        source ${./etc/aider}/aider-wrapper.bash
+        '';
+      }
+    )
+
+    (mkWrapperWithEnv "git" pkgs.git {
+      GIT_CONFIG_GLOBAL = gitconfig;
+    })
+    (mkWrapperWithEnv "rg" pkgs.ripgrep {
+      RIPGREP_CONFIG_PATH = ./etc/ripgrep/ripgrep.config;
+    })
+    (pkgs.writeShellApplication {
+      name = "jj";
+      runtimeInputs = [ pkgs.jujutsu pkgs.git pkgs.fzf ];
+      text = ''
+        export GIT_CONFIG_GLOBAL=${gitconfig}
+        export JJ_CONFIG=${jjconfig}:~/.config/jj/config.toml
+        exec jj "$@"
+      '';
+    })
+
+    (mkWrapperWithEnv "notmuch" pkgs.notmuch {
+      NOTMUCH_CONFIG = pkgs.replaceVars ./etc/notmuch/config {
+        hookDir = "${./etc/notmuch/hooks}";
+      };
+    })
+    pkgs.notmuch.emacs
+
+    (pkgs.librime.override {
+      plugins = [
+        (pkgs.librime-lua.override {
+          # https://github.com/iDvel/rime-ice/issues/840
+          lua = pkgs.lua5_4;
+        })
+      ];
+    })
+    pkgs.rime-ice
+
     pkgs.ast-grep
     pkgs.autojump
     pkgs.bazel-buildtools  # buildifier
@@ -48,22 +208,19 @@ pkgs.buildEnv {
     pkgs.bind.dnsutils
     pkgs.bubblewrap
     pkgs.clang-tools
-    # pkgs.claude-code
     pkgs.clickhouse
-    # pkgs.codex
     pkgs.cpplint
     (pkgs.curl.override { c-aresSupport = !pkgs.stdenv.isDarwin; })
     pkgs.docker-client
     pkgs.docker-compose
     pkgs.dtrx
+    pkgs.emacs-lsp-booster
     pkgs.fd
     pkgs.file
     pkgs.ffmpeg
     pkgs.flamegraph
     pkgs.fzf
     pkgs.gawk  # install as awk directly
-    # pkgs.gemini-cli
-    # pkgs.git
     pkgs.git-lfs
     pkgs.glab
     pkgs.gnupg
@@ -86,15 +243,16 @@ pkgs.buildEnv {
     pkgs.neovim
     pkgs.nmap
     pkgs.nodejs
-    # pkgs.notmuch
-    # pkgs.notmuch.emacs
     pkgs.pre-commit
     pkgs.pv
     pkgs.pwgen
     pkgs.pyright
     pkgs.python312Packages.httpie
     pkgs.python312Packages.markdown2
-    # pkgs.ripgrep
+    # yes, do not enable mount in linux.
+    # when enabled, it would use "fusermount3" in nix, which does not have setuid bit set.
+    # when disabled, the feature is still present, but it would use system's fusermount3
+    (pkgs.rclone.override { enableCmount = pkgs.stdenv.isDarwin; })
     pkgs.rsync
     pkgs.rustup
     pkgs.socat
@@ -108,110 +266,7 @@ pkgs.buildEnv {
     pkgs.whois
     pkgs.yubikey-manager
     pkgs.yubikey-personalization
-
-    # yes, do not enable mount in linux.
-    # when enabled, it would use "fusermount3" in nix, which does not have setuid bit set.
-    # when disabled, the feature is still present, but it would use system's fusermount3
-    (pkgs.rclone.override { enableCmount = pkgs.stdenv.isDarwin; })
-
-    (pkgs.librime.override {
-      plugins = [
-        (pkgs.librime-lua.override {
-          # https://github.com/iDvel/rime-ice/issues/840
-          lua = pkgs.lua5_4;
-        })
-      ];
-    })
-    pkgs.rime-ice
-
-    (
-      let python312 = pkgs.python312.override {
-            packageOverrides = final: prev: rec {
-              xonsh = prev.xonsh.overridePythonAttrs (prev: rec {
-                # https://github.com/xonsh/xonsh/pull/6026
-                version = "0.22.0-fix-completer";
-                src = pkgs.fetchFromGitHub {
-                  owner = "blahgeek";
-                  repo = "xonsh";
-                  rev = "1e239faed7a16e1b098acb503f1d884e719e8607";
-                  hash = "sha256-EQiK1d60F/rHX1K+S6KqpzL82ssulECeasSiAkQ+Ah0=";
-                };
-                doCheck = false;
-              });
-            };
-          };
-      in
-        (pkgs.xonsh.override {
-          python3 = python312;
-          extraPackages = ps: [
-            ps.xonsh.xontribs.xontrib-abbrevs
-            (with ps; buildPythonPackage {
-              pname = "xontrib-autojump";
-              version = "1.4";
-              src = pkgs.fetchFromGitHub {
-                owner = "wshanks";
-                repo = "xontrib-autojump";
-                tag = "v1.4";
-                hash = "sha256-IhF40olhMR5Ymu57kDu8jzD4QCjd6wMzHcsubNExpaA=";
-              };
-              pyproject = true;
-              build-system = [
-                setuptools
-              ];
-            })
-          ];
-        })
-    )
-
-    # (pkgs.rustPlatform.buildRustPackage rec {
-    #   pname = "jujutsu";
-    #   version = "0.39.0-master-lfs";
-    #   src = pkgs.fetchFromGitHub {
-    #     owner = "blahgeek";
-    #     repo = "jujutsu";
-    #     rev = "5e3a5acfe635c4ff6bbaba3fdb9d392ad586b5c9";  # 0.39.0-master-lfs
-    #     hash = "sha256-28+wlcA913fxJH3jtAXFh1oZm0uNTfJbbGPNSFrl9No=";
-    #   };
-    #   cargoHash = "sha256-R1ekt62wjM59qA2z22/2ljLmNAMHTzb1Ka1BG6ui3oc=";
-
-    #   doCheck = false;
-    #   cargoBuildFlags = [
-    #     # Don’t install the `gen-protos` build tool.
-    #     "--bin"
-    #     "jj"
-    #   ];
-    # })
-
-    (pkgs.rustPlatform.buildRustPackage rec {
-      pname = "emacs-lsp-booster";
-      version = "5f702a26";
-      src = pkgs.fetchFromGitHub {
-        owner = "blahgeek";
-        repo = "emacs-lsp-booster";
-        rev = "5f702a2699f306a3958ff1996a2b1a625f0cee0b";
-        hash = "sha256-R9v+hCma/FfYdR+fvZ0vmtVk4dm+bPBacwV1QCc6X+8=";
-      };
-      cargoHash = "sha256-qchwxW3KITQcv6EFzR2BSISWB2aTW9EdCN/bx5m0l48=";
-      doCheck = false;
-    })
-
-    # # kimi-cli. use project's flake and flake-compat
-    # (
-    #   let
-    #     projectSrc = fetchTarball {
-    #       url = "https://github.com/MoonshotAI/kimi-cli/archive/refs/tags/1.24.0.tar.gz";
-    #       sha256 = "1ds0w6m1nv2l84g0hd78ibgsy7sx0ys9qyrdcd1dgc3bzn6k5rm2";
-    #     };
-    #   in
-    #     (flake-compat { src = projectSrc; }).defaultNix.packages.${pkgs.stdenv.hostPlatform.system}.default
-    # )
+    pkgs.xonsh
   ]
-  ++ ((import ./lib/git.nix) { inherit pkgs; }).outs
-  ++ ((import ./lib/jj.nix) { inherit pkgs; }).outs
-  ++ ((import ./lib/notmuch.nix) { inherit pkgs; }).outs
-  ++ ((import ./lib/ripgrep.nix) { inherit pkgs; }).outs
-  ++ ((import ./lib/my-scripts.nix) { inherit pkgs; }).outs
-  ++ ((import ./lib/agent-tools.nix) { inherit pkgs; }).outs
-  ++ ((import ./lib/aider.nix) { inherit pkgs; }).outs
-  ;
+  ++ pkgs.lib.mapAttrsToList (_: v: v) myScripts;
 }
