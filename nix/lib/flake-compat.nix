12b5344a97e0c9318ac9afc23a90f1da40ae099f
Yikai Zhao <yikai@z1k.dev>
2026-04-02 01:01:06 +0800

    cleanup home.nix. single file

--- nix/lib/flake-compat.nix
+++
@@ -1,5 +0,0 @@
-import (fetchTarball {
-  # 2026.01.24
-  url = "https://github.com/edolstra/flake-compat/archive/5edf11c44bc78a0d334f6334cdaf7d60d732daab.tar.gz";
-  sha256 = "0yqfa6rx8md81bcn4szfp0hjq2f3h9i8zjzhqqyfqdkrj5559nmw";
-})
