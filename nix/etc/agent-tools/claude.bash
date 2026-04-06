a79e9a8ed74e6b073e08de019c6f5908d76cf195
Yikai Zhao <yikai@z1k.dev>
2026-04-06 10:50:09 +0800

    claude: do not use openrouter

--- nix/etc/agent-tools/claude.bash
+++ nix/etc/agent-tools/claude.bash
@@ -1,19 +1,15 @@
-if [ ! -f ~/.claude.json ]; then
-    echo '{ "hasCompletedOnboarding": true }' > ~/.claude.json
-fi
 
 if [[ -n "$INSIDE_STEALTH_INTERNAL" ]]; then
+    if [ ! -f ~/.claude.json ]; then
+        echo '{ "hasCompletedOnboarding": true }' > ~/.claude.json
+    fi
     echo "Inside stealth.internal, using q-stealth..."
     export ANTHROPIC_BASE_URL=https://o.a.stealth.internal/raw/vibe/
     API_KEY="$(emacs-get-gptel-api-key api.stealth.internal)"
-else
-    echo "Using OpenRouter with api_key $OPENROUTER_API_KEY"
-    export ANTHROPIC_BASE_URL="https://openrouter.ai/api"
-    API_KEY="$(emacs-get-gptel-api-key openrouter-claude-code)"
-fi
 
-export ANTHROPIC_AUTH_TOKEN="$API_KEY"
-export ANTHROPIC_API_KEY="" # Important: Must be explicitly empty
+    export ANTHROPIC_AUTH_TOKEN="$API_KEY"
+    export ANTHROPIC_API_KEY="" # Important: Must be explicitly empty
+fi
 
 sandbox_rw_files=("$HOME/.claude.json" "$HOME/.claude")
 sandbox_extra_args=(--dangerously-skip-permissions)
