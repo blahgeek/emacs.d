50b3367a78d778e6480361a265c43549c593e572
Yikai Zhao <yikai@z1k.dev>
2026-04-15 23:17:07 +0800

    move agent-skills and agents.md

--- nix/etc/agent-tools/kimi.bash
+++ nix/etc/agent-tools/kimi.bash
@@ -6,15 +6,11 @@ mkdir -p ~/.kimi
 export BROWSER=true
 
 sandbox_rw_files=("$HOME/.kimi")
-sandbox_extra_args=(--yolo)
 sandbox_skills_dir="$HOME/.agents/skills"
-sandbox_global_instruction_file="$HOME/.agents/AGENTS.md"
+# not required. defined in agent.yaml using relative path
+sandbox_global_instruction_file="/tmp/_notused_.md"
 
-_tmp_agent_yaml_file=$(mktemp /tmp/kimi.custom-agent.XXXXXX.yaml)
-cat <<EOF > $_tmp_agent_yaml_file
-version: 1
-agent:
-  extend: default  # 继承默认 Agent
-  system_prompt_path: $sandbox_global_instruction_file
-EOF
-sandbox_extra_args+=(--agent-file $_tmp_agent_yaml_file)
+sandbox_extra_args+=(
+    --yolo
+    --agent-file "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/kimi/agent.yaml"
+)
