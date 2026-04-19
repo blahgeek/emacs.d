d9db850e3d402033f386d311c6efcc454c1d10e4
Yikai Zhao <yikai@z1k.dev>
2026-04-19 08:52:33 +0800

    nix/pi.bash: switch to ~/.pi_sandbox, add auth.json, run offline

--- nix/etc/agent-tools/pi.bash
+++ nix/etc/agent-tools/pi.bash
@@ -1,19 +1,30 @@
-mkdir -p ~/.pi/sessions
+# ~/.pi 不会被真正的用到，因为PI_CODING_AGENT_DIR整个指定了
+# 但是有几个目录和文件会特别的指过去或者bind过去
+# 为了避免歧义，用~/.pi_sandbox目录
+
+mkdir -p ~/.pi_sandbox/sessions
+
+if [[ ! -f ~/.pi_sandbox/auth.json ]]; then
+    echo '{}' > ~/.pi_sandbox/auth.json
+fi
 
 # put settings in /pi/agent instead of ~/.pi/agent, to prevent home path in system prompt
 export PI_CODING_AGENT_DIR=/pi/agent
 
 sandbox_rw_files=(
-    "$HOME/.pi"
+    "$HOME/.pi/sessions"
     "$_MODELS_JSON:/pi/agent/models.json"
     "$SCRIPT_DIR/pi/agent/settings.json:/pi/agent/settings.json"
     "$SKILLS_DIR:/pi/agent/skills"
     "$SCRIPT_DIR/agents.md:/pi/agent/AGENTS.md"
+    "$HOME/.pi_sandbox/auth.json:/pi/agent/auth.json"
 )
 unset MODELS_JSON
 
 # session dir should still be in ~/.pi
 sandbox_extra_args=(
+    # disable update check
+    --offline
     --session-dir ~/.pi/sessions
 )
 
